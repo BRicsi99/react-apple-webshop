51d42cd6fee20b1222aa7f1e3b9ddc649c3ef92d
GenRichy <richard.bardos@genesys.com>
2024-01-04 13:21:40 +0100

    Folder structure & Tailwind setup

---
+++ components/layout/layout.tsx
@@ -0,0 +1,12 @@
+import classes from './Layout.module.css';
+
+function Layout() {
+  return (
+    <div>
+      <header></header>
+      <main className={classes.main}></main>
+    </div>
+  );
+}
+
+export default Layout;
---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import '@/styles/globals.css';
+import { AppProps } from "next/app";
+import Head from "next/head";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>NextJS TailwindCSS TypeScript Starter</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
