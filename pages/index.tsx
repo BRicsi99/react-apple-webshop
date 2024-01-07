96b8e4d2e9c708ede6bb031a88e26be12c35cf6c
bardosrichard99 <bardosrichard99@gmail.com>
2024-01-07 14:40:30 +0100

    Infinite scroll

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,19 @@ import { ProductProps } from '@/models/model';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const Home = () => {
   const [data, setData] = useState<ProductProps[]>([]);
+  const [dataShown, setDataShown] = useState<ProductProps[]>([]);
+  const [hasMore, setHasMore] = useState<boolean>(true);
 
   useEffect(() => {
     axios
       .get(`https://dummyjson.com/products`)
       .then(res => {
         setData(res.data.products);
+        setDataShown(res.data.products.slice(0, 12));
       })
       .catch(err => {
         toast.error('Failed to fetch products');
@@ -19,12 +23,40 @@ const Home = () => {
       });
   }, []);
 
+  const fetchMoreData = () => {
+    if ((dataShown.length >= data.length)) {
+      setHasMore(false);
+      return;
+    }
+    setTimeout(() => {
+      if (dataShown.length + 8 > data.length) {
+        setDataShown(
+          dataShown.concat(data.slice(dataShown.length, dataShown.length + (data.length - dataShown.length)))
+        );
+      }
+      setDataShown(dataShown.concat(data.slice(dataShown.length, dataShown.length + 8)));
+    }, 1500);
+  };
+
   return (
     <div className='items-center p-4 mt-4'>
       <main>
         <h1 className='text-[#323232] text-4xl font-semibold text-center'>See Products</h1>
-        <div className='flex flex-wrap my-12 mobile:px-0 px-6 w-full gap-1 justify-between'>
-          {!data ? <p>No products available</p> : data.map(p => <ProductCard key={p.id} data={p} />)}
+        <div className=' '>
+          <InfiniteScroll
+            dataLength={dataShown.length}
+            next={fetchMoreData}
+            className='flex flex-wrap my-12 mobile:px-0 px-6 w-full gap-1 justify-between'
+            hasMore={hasMore}
+            loader={<h4 className='w-full px-4'>Loading...</h4>}
+            endMessage={
+              <p className='text-center w-full'>
+                <b>This is the end of the product list</b>
+              </p>
+            }
+          >
+            {!data ? <p>No products available</p> : dataShown.map(p => <ProductCard key={p.id} data={p} />)}
+          </InfiniteScroll>
         </div>
       </main>
     </div>
