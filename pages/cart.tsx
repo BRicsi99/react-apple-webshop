c56f1995d4da181177dddc0fa4291301f0393728
bardosrichard99 <bardosrichard99@gmail.com>
2024-01-07 14:57:35 +0100

    Mobile view bug fix

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -52,28 +52,30 @@ const Cart = () => {
                 {cartItems.map((item: CartItem, index: number) => (
                   <div key={index}>
                     <div className='flex gap-2 items-center justify-between'>
-                      <img src={item.thumbnail} alt={item.title} className='w-32 h-32 rounded-lg object-cover' />
-                      <p className='text-lg text-black font-semibold text-center'>{item.title}</p>
-                      <div>
-                        <button
-                          className='bg-[#6100FF] rounded-l-lg my-2 px-2 w-min text-white cursor-pointer border-2 border-[#6100FF]'
-                          onClick={() => {
-                            onAmountChange(item.id, false);
-                          }}
-                        >
-                          -
-                        </button>
-                        <button className='bg-[#f2f4f8] my-2 px-2 w-min text-black cursor-auto border-2'>
-                          {item.quantity}
-                        </button>
-                        <button
-                          className='bg-[#6100FF] rounded-r-lg my-2 px-2 w-min text-white cursor-pointer border-2 border-[#6100FF]'
-                          onClick={() => {
-                            onAmountChange(item.id, true);
-                          }}
-                        >
-                          +
-                        </button>
+                      <img src={item.thumbnail} alt={item.title} className='w-2/5 aspect-square max-w-32 rounded-lg object-cover' />
+                      <div className='flex flex-wrap items-center justify-between w-3/5'>
+                        <p className='text-lg text-black font-semibold text-left'>{item.title}</p>
+                        <div className='flex flex-nowrap'>
+                          <button
+                            className='bg-[#6100FF] rounded-l-lg my-2 px-2 w-min text-white cursor-pointer border-2 border-[#6100FF]'
+                            onClick={() => {
+                              onAmountChange(item.id, false);
+                            }}
+                          >
+                            -
+                          </button>
+                          <button className='bg-[#f2f4f8] my-2 px-2 w-min text-black cursor-auto border-2'>
+                            {item.quantity}
+                          </button>
+                          <button
+                            className='bg-[#6100FF] rounded-r-lg my-2 px-2 w-min text-white cursor-pointer border-2 border-[#6100FF]'
+                            onClick={() => {
+                              onAmountChange(item.id, true);
+                            }}
+                          >
+                            +
+                          </button>
+                        </div>
                       </div>
                     </div>
                     {index < cartItems.length - 1 && <hr className='bg-gray-600 border-0 h-px mb-4 mt-6' />}
